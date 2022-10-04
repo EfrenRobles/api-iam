2c6d4548c2ad2ca2bb70931cff74106b0f08439f
Efren Robles <efren_robles@hotmail.com>
2022-10-04 11:34:57 -0700

    * Updating RoleService to return a specific object instead a ResponseEntity object.

--- src/main/java/api/iam/role/application/RoleService.java
+++ src/main/java/api/iam/role/application/RoleService.java
@@ -3,22 +3,20 @@ package api.iam.role.application;
 import java.util.UUID;
 
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
-
 import api.iam.role.domain.request.AddRoleRequest;
 import api.iam.role.domain.request.UpdateRoleRequest;
 import api.iam.role.domain.response.RoleResponse;
-import api.shared.application.PageService;
+import api.shared.domain.response.PaginationResponse;
 
 public interface RoleService {
 
-	public ResponseEntity<?> getRole(UUID roleId) throws Exception;
+    public RoleResponse getRole(UUID roleId) throws Exception;
 
-	public ResponseEntity<?> getAllRole(Pageable pageable, RoleResponse role);
+    public PaginationResponse getAllRole(Pageable pageable, RoleResponse role);
 
-	public ResponseEntity<?> addRole(AddRoleRequest request) throws Exception;
+    public RoleResponse addRole(AddRoleRequest request) throws Exception;
 
-	public ResponseEntity<?> updateRole(UpdateRoleRequest request) throws Exception;
-	
-	public ResponseEntity<?> deleteRole(UUID roleId) throws Exception;
+    public RoleResponse updateRole(UpdateRoleRequest request) throws Exception;
+
+    public UUID deleteRole(UUID roleId) throws Exception;
 }
