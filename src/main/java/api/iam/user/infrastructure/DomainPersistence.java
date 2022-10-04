aa597dab1fc1bb437a6cda85f28d669b57f4296b
Efren Robles <efren_robles@hotmail.com>
2022-10-03 19:10:11 -0700

    - Removing DomainPersistence, deprecated. + Adding com.vladmihalcea lib to handle jsonb from postgres. * Updating migration to replace role by role_id on 09_create_user_clients_table_30_09_2022.sql

--- src/main/java/api/iam/user/infrastructure/DomainPersistence.java
+++
@@ -1,7 +0,0 @@
-package api.iam.user.infrastructure;
-
-import api.iam.user.domain.UserRepository;
-
-public interface DomainPersistence extends UserRepository {
-
-}
--- src/main/resources/postgreSql/09_create_user_clients_table_30_09_2022.sql
+++ src/main/resources/postgreSql/09_create_user_clients_table_30_09_2022.sql
@@ -1,9 +1,10 @@
-CREATE TABLE public.user_clients (
+CREATE TABLE user_clients (
 	user_id uuid NOT NULL,
 	client_id uuid NOT NULL,
-	"role" varchar NOT NULL,
+	role_id uuid NOT NULL,
 	scopes jsonb NOT NULL,
 	CONSTRAINT client_scopes_pk PRIMARY KEY (user_id,client_id),
-	CONSTRAINT client_scopes_fk FOREIGN KEY (user_id) REFERENCES public.users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
-	CONSTRAINT client_scopes_fk_1 FOREIGN KEY (client_id) REFERENCES public.clients(client_id) ON DELETE CASCADE ON UPDATE CASCADE
+	CONSTRAINT client_scopes_fk FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT client_scopes_fk_1 FOREIGN KEY (client_id) REFERENCES clients(client_id) ON DELETE CASCADE ON UPDATE cascade,
+	CONSTRAINT client_scopes_fk_2 FOREIGN KEY (role_id) REFERENCES roles(role_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
